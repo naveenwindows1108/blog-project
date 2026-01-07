badbe639c70134a5643c4939968441a2f302f86b
Naveen Windows <naveenwindows1108@gmail.com>
2026-01-07 16:30:25 +0530

    updated views using generics

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
+    path('home/', views.HomeAPI.as_view(), name='blog-home'),
+    path('post/<int:pk>/', views.HomePostAPI.as_view(), name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
 from .models import Post
-from django.contrib.auth.models import User
 from .serializers import PostSerializer
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import generics
+
+class HomeAPI(generics.ListAPIView):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializer
 
 
-@api_view(['GET'])
-def home(request):
-    if request.method == "GET":
-        post = Post.objects.all()
-        serializer = PostSerializer(post, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class HomePostAPI(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializer
+    
 
 
-def about(request):
-    return JsonResponse({'page': 'about'})
 
-# Create your views here.
